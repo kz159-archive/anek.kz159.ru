6aae3d7f7064b662159776dd6ab6a67624c037a6
kz159 <loh@kz159.ru>
2018-11-29 11:20:46 +0000

    removed some debug options, added authors

--- base.py
+++ base.py
@@ -10,24 +10,25 @@ PORT = 8080 #default port
 
 f_string = ['Подходит поручик Ржевский к Наташе Ростовой', "Купил мужик шляпу", "подходит мужчина к гинекологу"]
 s_string = [' а она ему как раз', ' сел в машину и сгорел', ' а там армяне в нарды играют', ' а она до самого хуя']
+authors = ['Navalny', 'Gitlar', 'Albert Einstein', 'Nikola Tesla', 'Alan Turing', 'Benedict Cucumberpech']
 
-print (os.path.abspath(os.getcwd()))
-
-def html_gen(string):
+def html_gen(string, author):
     return """<html>
           <head>
 	  <link href="/static/css/style.css" rel="stylesheet">
 	  </head>
           <body>
           <h2 style="text-align: center;"><font color = "white">{}</font></h2>
+          <p style="text-align:right;"><cite>&ndash;{}</cite></p>
           </body>
-        </html>""".format(string)
+        </html>""".format(string, author)
 
 class Root(object):
     @cherrypy.expose
     def index(self):
         string = random.choice(f_string) + random.choice(s_string)
-        return html_gen(string)
+        author = random.choice(authors)
+        return html_gen(string, author)
 
 if __name__ == '__main__':
     conf ={
@@ -45,4 +46,5 @@ if __name__ == '__main__':
                 'tools.staticdir.dir': './public',
                 }
 }
+
 cherrypy.quickstart(Root(), '/', conf)
