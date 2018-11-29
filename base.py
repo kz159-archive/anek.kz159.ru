dbc354f1822ba78555440b0872ac3a999924bdd8
karaz159 <karaz91@mail.ru>
2018-11-29 10:50:54 +0300

    rebuild for proxying all to nginx

--- base.py
+++ base.py
@@ -5,33 +5,40 @@ import cherrypy
 f_string = ['Подходит поручик Ржевский к Наташе Ростовой', "Купил мужик шляпу", "подходит мужчина к гинекологу"]
 s_string = [' а она ему как раз', ' сел в машину и сгорел', ' а там армяне в нарды играют', ' а она до самого хуя']
 
+print (os.path.abspath(os.getcwd()))
+
 def html_gen(string):
     return """<html>
           <head>
-          <link href="/static/css/style.css" rel="stylesheet">
-          </head>
+	  <link href="/static/css/style.css" rel="stylesheet">
+	  </head>
           <body>
           <h2 style="text-align: center;"><font color = "white">{}</font></h2>
           </body>
         </html>""".format(string)
 
-
-
-class StringGenerator(object):
+class Root(object):
     @cherrypy.expose
     def index(self):
         string = random.choice(f_string) + random.choice(s_string)
         return html_gen(string)
 
 if __name__ == '__main__':
-    conf = {
+    conf ={
+	    'global':{
+		'server.socket_port': 8080,
+		},
             '/': {
                 'tools.sessions.on': True,
+	        'tools.proxy.on': True,
+        	'tools.proxy.base': 'http://anek.kz159.ru',
                 'tools.staticdir.root': os.path.abspath(os.getcwd())
                 },
-                '/static': {
+            '/static': {
                 'tools.staticdir.on': True,
-                'tools.staticdir.dir': './public'
+                'tools.staticdir.dir': './public',
+		'tools.staticdir.debug':True,
+		'log.sreen': True
                 }
-            }
-    cherrypy.quickstart(StringGenerator(), '/', conf)
+}
+cherrypy.quickstart(Root(), '/', conf)
